385f5081b0c4313a431a48ea7f0a903b9fba1f8a
yundianzixun <zhenghhgz@163.com>
2019-07-31 14:26:55 +0800

    增加Eureka Server集群

---
+++ microservice-discovery-eureka-ha/src/main/java/com/itunion/cloud/MicroserviceDiscoveryEurekaHaApplication.java
@@ -0,0 +1,20 @@
+package com.itunion.cloud;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @Auther: Lin
+ * @Date: 2019-07-29 23:01
+ * @Description: 使用Eureka做服务发现
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class MicroserviceDiscoveryEurekaHaApplication {
+    public static void main(String[] args){
+        SpringApplication.run(MicroserviceDiscoveryEurekaHaApplication.class,args);
+    }
+
+}
