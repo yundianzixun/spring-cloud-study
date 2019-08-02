6c19a5c386ee71302a21eedc604f191883aaf6c0
yundianzixun <zhenghhgz@163.com>
2019-08-02 11:43:59 +0800

    04-Spring Cloud集成Security（认证），并将微服务注册到Eureka Server上

---
+++ microservice-consumer-goods-security/src/main/java/com/itunion/cloud/MicroserviceConsumerGoodsSecurityApplication.java
@@ -0,0 +1,20 @@
+package com.itunion.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class MicroserviceConsumerGoodsSecurityApplication {
+	//实例化一个RestTemplate 名字叫restTemplate
+	@Bean
+	public RestTemplate restTemplate(){
+		return new RestTemplate();
+	};
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceConsumerGoodsSecurityApplication.class, args);
+	}
+
+}
---
+++ microservice-discovery-eureka-security/src/main/java/com/itunion/cloud/MicroserviceDiscoveryEurekaSecurityApplication.java
@@ -0,0 +1,31 @@
+package com.itunion.cloud;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * @Auther: Lin
+ * @Date: 2019-07-29 23:01
+ * @Description: 使用Eureka做服务发现
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class MicroserviceDiscoveryEurekaSecurityApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviceDiscoveryEurekaSecurityApplication.class, args);
+    }
+
+    @EnableWebSecurity
+    static class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http.csrf().ignoringAntMatchers("/eureka/**");
+            super.configure(http);
+        }
+    }
+}
